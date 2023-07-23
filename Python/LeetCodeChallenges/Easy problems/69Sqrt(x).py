4d62ea137c6de6288edeefac3b35a5869d1cba39
preethamdheeraj <chpreetham45@gmail.com>
2023-07-23 12:58:50 +0530

    ADD leetcode challenges in python

--- Python/LeetCodeChallenges/Easy problems/69Sqrt(x).py
+++ Python/LeetCodeChallenges/Easy problems/69Sqrt(x).py
@@ -3,6 +3,7 @@ from typing import List
 
 class Solution:
     def mySqrt(self, x: int) -> int:  #  x is non negative integer
+        # in this example we have not used any built in module or function
         if x == 0:
             return 0
 
